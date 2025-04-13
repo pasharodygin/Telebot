72c28e2887f0d7ecaedd81ff11b4f08d89238a3c
prog <rodyginpaul@ya.ru>
2025-04-13 20:53:32 +0300

    config.py modified

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 TOKEN = '5735861936:AAHj33x3tkPtLAkNIgvpo-Hbuk1ZhAZ5qB8'
-URL1 = "https://postypashki.ru/ecwd_calendar/calendar/"
-URL2 = "https://www.anekdot.ru/last/good/"
+URL1 = "http://postypashki.ru/ecwd_calendar/calendar/"
+URL2 = "http://www.anekdot.ru/last/good/"
+
